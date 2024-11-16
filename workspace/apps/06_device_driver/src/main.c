b7f79941e3ddaee6d573fd163b2e76f56605ce5c
ShawnHymel <5232145+ShawnHymel@users.noreply.github.com>
2024-11-16 09:36:39 -0700

    Changed device driver directory names

--- workspace/apps/06_device_driver/src/main.c
+++ workspace/apps/06_device_driver/src/main.c
@@ -1,50 +1,51 @@
 #include <stdio.h>
 #include <zephyr/kernel.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/sensor.h>
+
+#include "button.h"
 
 // Settings
-static const int32_t sleep_time_ms = 1000;
-
-// Get Devicetree configurations
-static const struct device *const mcp = DEVICE_DT_GET(DT_ALIAS(my_mcp9808));
+static const int32_t sleep_time_ms = 50;
+static const struct device *btn_1 = DEVICE_DT_GET(DT_ALIAS(my_button_1));
+static const struct device *btn_2 = DEVICE_DT_GET(DT_ALIAS(my_button_2));
 
 int main(void)
 {
-	
-	int ret;
+    int ret;
+    uint8_t state_1;
+    uint8_t state_2;
 
-    // Check if the MCP9808 has been initialized (init function called)
-	if (!device_is_ready(mcp)) {
-		printk("Device %s is not ready.\n", mcp->name);
-		return 0;
-	}
+    // Make sure that the button was initialized
+    if (!(device_is_ready(btn_1) && device_is_ready(btn_2))) {
+        printk("Error: button not ready\r\n");
+        return 0;
+    }
+
+    // Get the API from one of the buttons
+    const struct button_api *btn_api = (const struct button_api *)btn_1->api;
 
     // Do forever
-    while (1) 
-    {
-        struct sensor_value tmp;
-
-        // Fetch the temperature value from the sensor into the device's data structure
-        ret = sensor_sample_fetch(mcp);
+    while (1) {
+        
+        // Poll button 1 state
+        ret = btn_api->get(btn_1, &state_1);
         if (ret < 0) {
-            printk("Sample fetch error: %d\n", ret);
-            return 0;
+            printk("Error %d: failed to read button 1 pin\r\n", ret);
+            continue;
         }
 
-        // Copy the temperature value from the device's data structure into the tmp struct
-        ret = sensor_channel_get(mcp, SENSOR_CHAN_AMBIENT_TEMP, &tmp);
+        // Poll button 2 state
+        ret = btn_api->get(btn_2, &state_2);
         if (ret < 0) {
-            printk("Channel get error: %d\n", ret);
-            return 0;
+            printk("Error %d: failed to read button 2 pin\r\n", ret);
+            continue;
         }
 
-        // Print the temperature value 
-        printk("Temperature: %d.%06d\n", tmp.val1, tmp.val2);
+        // Print button states
+        printk("%u %u\r\n", state_1, state_2);
 
         // Sleep
         k_msleep(sleep_time_ms);
     }
-    
+
     return 0;
 }
